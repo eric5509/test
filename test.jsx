ffb5061c41c7830dad8795762e6446ce5a1133ae
Eric <38852525+eric5509@users.noreply.github.com>
2023-01-01 10:01:19 +0100

    Add files via upload

---
+++ test.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+
+const Test = () => {
+    const [products, setProducts] = useState([
+        {
+            id: 1,
+            name: 'Mac Book Pro 16',
+            productId: '123ABC',
+            discountPrice: 320000,
+            actualPrice: 300000,
+            rating: 4.5,
+        },
+        {
+            id: 2,
+            name: 'iPhone 13 Pro Max',
+            productId: '122ABC',
+            discountPrice: 270000,
+            actualPrice: 250000,
+            rating: 7.5,
+        },
+    ])
+    const [itemDetails, setItemDetails] = useState({})
+    const getItemDetails = (productId) => {
+    const newProduct = products.filter(product => { return product.productId === productId})
+    const singleProduct = newProduct[0]
+        setItemDetails(singleProduct)
+        console.log(itemDetails)
+    }
+
+    useEffect(() => {
+        getItemDetails('123ABC')
+    }, [])
+    return ( 
+        <div className="flex"></div>
+     );
+}
+ 
+export default Test;
